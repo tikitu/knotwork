1ffc4527bce4cbbd080202e23d9ee1253985c436
Tikitu de Jager <tikitu@minddistrict.com>
2017-02-20 00:28:56 +0200

    Get Boston-Baden's algorithm working

--- Knotwork.playground/Pages/Boston-Baden.xcplaygroundpage/Contents.swift
+++ Knotwork.playground/Pages/Boston-Baden.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@ struct Grid {
 let g = Grid(width: 5, height: 4)
 
 enum Tile: String {
-    case a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
+    case a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, empty = "@"
 
     var image: UIImage {
         return UIImage(imageLiteralResourceName: "\(self.rawValue).gif")
@@ -43,5 +43,168 @@ func draw(_ grid: [[Tile]]) -> UIImage {
 }
 
 draw(aGrid)
+
+var grid: [[Tile]] = [
+    [.l, .w, .l, .w, .l, .w, .l, .w],
+    [.u, .i, .u, .i, .u, .i, .u, .i],
+    [.l, .w, .l, .w, .l, .w, .l, .w],
+    [.u, .i, .u, .i, .u, .i, .u, .i],
+    [.l, .w, .l, .w, .l, .w, .l, .w],
+    [.u, .i, .u, .i, .u, .i, .u, .i],
+]
+
+let base = grid
+
+draw(grid)
+
+func topBottom(cell: Tile) -> Tile {
+    switch cell {
+    case .l: return .o
+    case .w: return .v
+    case .u: return .s
+    case .i: return .j
+    default:
+        return cell
+    }
+}
+
+grid[0] = grid.first!.map(topBottom)
+grid[grid.count - 1] = grid.last!.map(topBottom)
+
+draw(grid)
+
+func leftRight(_ cell: Tile) -> Tile {
+    switch cell {
+    case .l: return .k
+    case .u: return .b
+    case .w: return .y
+    case .i: return .f
+    default:
+        return cell
+    }
+}
+
+func leftRight(row: [Tile]) -> [Tile] {
+    var row = row
+    row[0] = leftRight(row[0])
+    row[row.count - 1] = leftRight(row.last!)
+    return row
+}
+
+grid = grid.map(leftRight)
+
+draw(grid)
+
+grid[0][0] = .n
+grid[0][grid[0].count - 1] = .x
+grid[grid.count - 1][0] = .a
+grid[grid.count - 1][grid[0].count - 1] = .g
+
+draw(grid)
+
+enum BreakLine {
+    case h, v, o
+}
+
+let antigrid: [[BreakLine]] = [
+    [.h, .h, .h, .h],
+    [.v, .o, .h, .o, .v],
+    [.o, .o, .o, .o],
+    [.v, .v, .v, .v, .v],
+    [.o, .o, .o, .o],
+    [.v, .o, .h, .o, .v],
+    [.h, .h, .h, .h],
+]
+
+enum Edge {
+    case n, s, w, e
+}
+
+func edges(x: Int, y: Int) -> Set<Edge> {
+    let odd = y % 2 == 1
+    let above = antigrid[y][(x + (odd ? 1 : 0)) / 2]
+    let below = antigrid[y+1][(x + (odd ? 0 : 1)) / 2]
+    var edges = Set<Edge>()
+    switch above {
+    case .h:
+        edges.insert(.n)
+    case .v:
+        edges.insert((x + y) % 2 == 0 ? .e : .w)
+    case .o:
+        break
+    }
+    switch below {
+    case .h:
+        edges.insert(.s)
+    case .v:
+        edges.insert((x + y) % 2 == 0 ? .w : .e)
+    case .o:
+        break
+    }
+    return edges
+}
+
+edges(x: 7, y: 5)
+
+func lookup(_ cell: Tile, with edges: Set<Edge>) -> Tile {
+    switch edges {
+    case []: return cell
+    case [.n, .w]: return .n
+    case [.n, .e]: return .x
+    case [.s, .w]: return .a
+    case [.s, .e]: return .g
+    case [.n, .s]: return .q
+    case [.w, .e]: return .d
+    case [.n]:
+        switch cell {
+        case .l: return .o
+        case .w: return .v
+        case .i: return .p
+        case .u: return .t
+        default:
+            fatalError()
+        }
+    case [.s]:
+        switch cell {
+        case .u: return .s
+        case .i: return .j
+        case .w: return .r
+        case .l: return .m
+        default:
+            fatalError()
+        }
+    case [.w]:
+        switch cell {
+        case .l: return .k
+        case .w: return .c
+        case .u: return .b
+        case .i: return .h
+        default:
+            fatalError()
+        }
+    case [.e]:
+        switch cell {
+        case .l: return .e
+        case .w: return .y
+        case .u: return .z
+        case .i: return .f
+        default:
+            fatalError()
+        }
+    default: return .empty
+    }
+}
+
+var newGrid: [[Tile]] = []
+for (y, row) in base.enumerated() {
+    var newRow = [Tile]()
+    for (x, cell) in row.enumerated() {
+        let newCell = lookup(cell, with: edges(x: x, y: y))
+        newRow.append(newCell)
+    }
+    newGrid.append(newRow)
+}
+
+draw(newGrid)
 
 //: [Next](@next)
