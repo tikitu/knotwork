85083de44d0896fb4e810b979c5fa2520301004e
Tikitu de Jager <tikitu@minddistrict.com>
2017-02-19 21:32:09 +0200

    Split playground to pages

---
+++ Knotwork.playground/Pages/Boston-Baden.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+struct Grid {
+    let width: Int
+    let height: Int
+}
+
+let g = Grid(width: 5, height: 4)
+
+//: [Next](@next)
